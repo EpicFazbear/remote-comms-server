2cb99b44bccf6ef53be9608a5b06d66dbe9b770a
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-03-28 22:33:56 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -45,15 +45,15 @@ local commands = { -- Our list of commands
 	end};
 
 	{Name = "append", Run = function(message)
-		file = io.open("log.txt", "a")
+		file = io.open("Edd.txt", "a")
 		-- sets the default output file as test.lua
 		io.output(file)
 		-- appends a word test to the last line of the file
-		io.write(" -- End of the log.txt file")
+		io.write(" -- End of the Edd.txt file")
 		-- closes the open file
 		io.close(file)
 
-		file = io.open("log.txt", "r")
+		file = io.open("Edd.txt", "r")
 		-- sets the default input file as test.lua
 		io.input(file)
 		-- prints the first line of the file
