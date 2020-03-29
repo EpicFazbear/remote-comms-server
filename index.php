419e1dbfc48076bcbd60efd0af819b366c45190c
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-03-28 22:46:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-  $myfile = fopen("Edd.txt", "r");
-  echo fread($myfile, filesize("Edd.txt"));
+  $myfile = fopen("Edd.txt", "a");
+  fwrite($myfile, "\nHello! Hello! Hello! Hello! How Low?");
   fclose($myfile);
+  $myfile2 = fopen("Edd.txt", "r");
+  echo fread($myfile2, filesize("Edd.txt"));
+  fclose($myfile2);
   //echo("I am learning PHP");
   //header("Location: index.html");
 ?>
