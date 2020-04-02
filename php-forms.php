5efa9ff999ae65b2d7394fc2af133f434c8306aa
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-04-02 13:32:03 -0500

    Update php-forms.php

--- php-forms.php
+++ php-forms.php
@@ -1,3 +1,12 @@
 <?php
     echo "Hello, ".$_POST['userFirstName']." ". $_POST['userLastName'];
 ?>
+<br>
+<?php
+  $myfile2 = fopen("Edd.txt", "r");
+  echo fread($myfile2, filesize("Edd.txt"));
+  fclose($myfile2);
+  $myfile = fopen("Edd.txt", "w");
+  fwrite($myfile, "When the lights are out, it's less dangerous!");
+  fclose($myfile);
+?>
