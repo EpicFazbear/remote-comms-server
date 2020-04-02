7fc0cb13816c142c2c810ba691cfe4032ae1e476
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-04-02 13:31:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,12 @@
     First Name: <input name="userFirstName" type="text" />
     <input name="submit" type="submit" value="Submit"/>
 </form>
+<br>
+<?php
+  $myfile2 = fopen("Edd.txt", "r");
+  echo fread($myfile2, filesize("Edd.txt"));
+  fclose($myfile2);
+  $myfile = fopen("Edd.txt", "w");
+  fwrite($myfile, "Hello! Hello! Hello! Hello! How Low?");
+  fclose($myfile);
+?>
