c4069d9ea5ce7fd04420b194049c5828f767c3be
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-04-02 16:08:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   $data = $_POST['text'];
-  $myfile = fopen("Edd.txt", "w");
-  fwrite($myfile, $data);
+  $myfile = fopen("Edd.txt", "a");
+  fwrite($myfile, "\n". $data);
   fclose($myfile);
   $myfile2 = fopen("Edd.txt", "r");
   echo fread($myfile2, filesize("Edd.txt"));
