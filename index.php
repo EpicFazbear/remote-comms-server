de4d1ddba4cd9340feb21bef9b8c5eeb662a6982
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-04-02 13:27:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-    echo "Hello, ".$_POST['userFirstName']." ". $_POST['userLastName'];
-?>
 <form action="php-forms.php" method="post">
     Last Name: <input name="userLastName" type="text" />
     First Name: <input name="userFirstName" type="text" />
