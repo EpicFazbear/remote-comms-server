6d3ff1d4f51a1dfee3e13c10cafbada8dd67b531
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-04-02 15:16:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<form action="php-forms.php" method="post">
-    Last Name: <input name="userLastName" type="text" />
-    First Name: <input name="userFirstName" type="text" />
-    <input name="submit" type="submit" value="Submit"/>
-</form>
-<br>
 <?php
-  $myfile2 = fopen("Edd.txt", "r");
-  echo fread($myfile2, filesize("Edd.txt"));
-  fclose($myfile2);
+  $data = $_POST['text'];
   $myfile = fopen("Edd.txt", "w");
-  fwrite($myfile, "Hello! Hello! Hello! Hello! How Low?");
+  fwrite($myfile, $data);
   fclose($myfile);
+  echo json_encode($data);
 ?>
