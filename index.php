af7963509c0e7f5275a8fcf73b9624118d8303b3
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-05-01 11:24:09 -0500

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  header('Content-Type: application/json');
-  $data = $_POST["content"];
-  echo($data);
-  $myfile = fopen("Edd.txt", "w");
-  fwrite($myfile, $data);
-  fclose($myfile);
-?>
