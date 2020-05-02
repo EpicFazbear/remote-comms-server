da547a5dec516d2db1b9f3631fe720416f7838a8
leSkehletron <EpicFazbear@users.noreply.github.com>
2020-05-02 09:29:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	// collect value of input field
 	// $data = json_decode(file_get_contents("php://input"));
 	// $name = $data["content"];
-	$result = $_REQUEST['rawRequest'];
-	$obj = json_decode($result, true);
 	$obj2 = json_decode($_REQUEST, true);
-	print($obj);
-	print($obj2);
+	print_r($obj2);
+	print("charlie");
 //	if (empty($name)) {
 //		echo "Name is empty";
 //	} else {
