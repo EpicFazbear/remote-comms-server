fe9aa3360b4607555e0cafa9992cba6a79fa4d30
leSkehletron <EpicFazbear@users.noreply.github.com>
2020-05-02 09:41:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	// collect value of input field
-	// $data = json_decode(file_get_contents("php://input"));
-	// $name = $data["content"];
-	echo file_get_contents("php://input");
+
+//get the data
+$json = file_get_contents("php://input");
+
+//convert the string of data to an array
+$data = json_decode($json, true);
+
+//output the array in the response of the curl request
+print_r($data);
+
 //	if (empty($name)) {
 //		echo "Name is empty";
 //	} else {
