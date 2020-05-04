e22864df8ad45239f7219559cffec77c95fc9363
leSkehletron <EpicFazbear@users.noreply.github.com>
2020-05-04 15:40:19 -0500

    New update (p1)

--- communicator.php
+++ communicator.php
@@ -5,9 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$json = file_get_contents("php://input");
 	$content = json_decode($json, true)["content"];
 
-	if (empty($content)) {
-		echo "Invalid parameters.";
-	} else {
+	if (empty($content)) {// Also check if theres a ["command"]
+		echo "Invalid parameters."; // Probably log these commands as well
+	} else { // Commands are not Adonis Commands, but instructions for the server scripts to do stuff (such as stop for a bit, etc.)
 		$stored = $json;
 		echo $stored;
 //		$file = fopen("Edd.txt", "w");
@@ -21,4 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
 	echo "Invalid parameters.";
 };
+// make the buffer ($stored) a table that stores all the messages sent until the server script GetRequests the php, in which
+// all of the data is sent, and the buffer is reset
+// if the buffer == DEFAULT, then server script doesn't do anything
 ?>
