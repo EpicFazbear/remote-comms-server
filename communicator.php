94c14e0b7e7677b11b4cb14929638a5fde782546
leSkehletron <EpicFazbear@users.noreply.github.com>
2020-05-04 23:52:51 -0500

    Update communicator.php

--- communicator.php
+++ communicator.php
@@ -4,13 +4,18 @@ $stored = "Hello! Hello! Hello! Hello! How Low?";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$recieved = json_decode(file_get_contents("php://input"));
 
-	if (empty($recieved["content"])) {// Also check if theres a ["command"]
-		echo "Invalid parameters."; // Probably log these commands as well
+	if (empty($recieved["content"])) {
+		if (!empty($recieved["command"])) {
+			$stored = $recieved["command"];
+			echo "Ran command."; // Probably log these commands as well
+		} else {
+			echo "Invalid parameters.";
+		};
 	} else { // Commands are not Adonis Commands, but instructions for the server scripts to do stuff (such as stop for a bit, etc.)
 		if (gettype($stored) != "array") {
 			$stored = {};
 		};
-		array_push($stored, $json)
+		array_push($stored, $json);
 		echo $stored;
 //		$file = fopen("Edd.txt", "w");
 //		fwrite($file, $recieved);
@@ -19,12 +24,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	};
 
 } elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
-	if (gettype($stored) == "array") {
-		echo json_encode($stored);
+	if (gettype($stored) == "array") { // Someone could theoretically hijack this system by spamming GET requests here and constantly clear the buffer
+		echo json_encode($stored); // Before server scripts could ever read them..
+		$stored = {};
 	} else {
 		echo $stored;
 	};
-	$stored = {};
 } else {
 	echo "Invalid parameters.";
 };
