79d7d2a09f13576ff00be862a5e1e01fd035db95
leSkehletron <EpicFazbear@users.noreply.github.com>
2020-05-27 17:38:05 -0500

    5/27 Update 2

--- communicator.php
+++ communicator.php
@@ -13,28 +13,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			echo "Invalid parameters.";
 		};
 	} else { // Commands are not Adonis Commands, but instructions for the server scripts to do stuff (such as stop for a bit, etc.)
-		if (gettype($stored) != "array") {
-			$stored = [];
-		};
-		array_push($stored, $json);
-		echo $stored;
+//		if (gettype($stored) != "array") {
+//			$stored = [];
+//		};
+//		array_push($stored, $json);
+//		echo $stored;
 
-//		$file = fopen("Edd.txt", "w");
-//		fwrite($file, $recieved);
-//		fclose($file);
-//		echo readfile("Edd.txt");
+		$stored = $json
 	};
 
 } elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
-	if (gettype($stored) == "array") { // Someone could theoretically hijack this system by spamming GET requests here and constantly clear the buffer
-		echo json_encode($stored); // Before server scripts could ever read them..
-		$stored = [];
-	} else {
+//	if (gettype($stored) == "array") { // Someone could theoretically hijack this system by spamming GET requests here and constantly clear the buffer
+//		echo json_encode($stored); // Before server scripts could ever read them..
+//		$stored = [];
+//	} else {
 		echo $stored;
-	};
+//	};
 } else {
 	echo "Invalid parameters.";
 };
 // Very far off future: Server scripts authenticate with a random GUID, and the communicator creates a secure and
-// separate buffer for them. When that server shuts down, it sends that to the server so that the GUID is removed.
+// separate buffer for them. When that server shuts down, it sends a message to the server so the GUID gets removed.
 ?>
